4126a4d837d0aadeef1896154646f8eb5bba3465
MaxsimTT <egorovcotmax@gmail.com>
2022-11-17 09:34:42 +0400

    deleted old migation and added new migration post table

---
+++ database/migrations/2022_11_17_051656_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('posts')) {
+            Schema::create('posts', function (Blueprint $table) {
+                $table->id();
+                $table->integer('category_post_id');
+                $table->string('name', 100);
+                $table->longText('description');
+                $table->string('author', 100);
+                $table->timestamp('create_date');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
